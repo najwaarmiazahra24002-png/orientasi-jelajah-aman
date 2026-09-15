6e93ef8dc50406e3c7280a374176df866c2007d4
najwaarmiazahra24002-png <najwa.armia.zahra24002@mhs.uingusdur.ac.id>
2026-09-15 12:09:28 +0700

    Update Laporan Udara

--- types/cuaca.ts
+++ types/cuaca.ts
@@ -1,13 +1,22 @@
 // types/cuaca.ts
 export interface DataCuaca {
-kota: string;
-suhu: number;
-kelembapan: number;
-catatan?: string;
+    kota: string;
+    suhu: number;
+    kelembapan: number;
+    catatan?: string;
 }
 export type TingkatAQI = "BAIK" | "SEDANG" | "TIDAK_SEHAT" | "BERBAHAYA";
+
 export interface WeatherCardProps {
-kota: string;
-suhu: number;
-tingkatAQI: TingkatAQI;
+    kota: string;
+    suhu: number;
+    tingkatAQI: TingkatAQI;
+}
+
+// Laporan Udara, Latihan Mandiri 2
+export interface LaporanUdara {
+    kota: string;
+    indeksAQI: number;
+    tingkat: TingkatAQI;
+    diperbaruiPada?: string;
 }
